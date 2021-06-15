4a33bd957034262ead42a78b6bf579a8f6dea351
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2021-06-15 17:23:19 -0300

    Some logs

--- desicion_tree.py
+++ desicion_tree.py
@@ -8,10 +8,15 @@ from math import log2
 import pandas as pd
 
 def getEntropyFromQuantities(listOfInt):
+    log = f'E{tuple(listOfInt)} = '
     total = sum(listOfInt)
     proportions = [value/total for value in listOfInt]
     proportionsMultipliedByLog2 = [p * log2(p) for p in proportions]
+
     sumatory = sum(proportionsMultipliedByLog2)
+
+    log += ' + '.join([f'{proportions} * log_2({proportion})' for proportion in proportions]) + f'\n= {sumatory}'
+    print(log)
     return -sumatory
 
 def getEntropy(dataset):
@@ -21,6 +26,8 @@ def getEntropy(dataset):
     return getEntropyFromQuantities(list(valueCounts.values))
 
 def informationGain(datasetDataFrame, attribute):
+    totalEntropy = getEntropy(datasetDataFrame)
+    log = f'Gain(S, {attribute}) = E(S) - sum( |Sv| / |S| * E(Sv) )\n= {totalEntropy}'
     values = datasetDataFrame[attribute].unique()
     
     summatory = 0
@@ -29,15 +36,15 @@ def informationGain(datasetDataFrame, attribute):
         entropy = getEntropy(datasetFilteredByValue)
         proportionOverTotal = len(datasetFilteredByValue) / len(datasetDataFrame)
         summatory += proportionOverTotal * entropy
+        log += f' - {proportionOverTotal} * {entropy}'
     
-    return getEntropy(datasetDataFrame) - summatory
+    result = totalEntropy - summatory
+    log += f'\n= {result}'
+    print(log)
+    return result
 
 #%%
 df = pd.read_csv('dataset.csv')
 
-print('Entropy(S) = ', getEntropy(df))
-print('Gain(S, Humidity) = ', informationGain(df, 'Humidity'))
-
-# %%
 assert(getEntropy(df) == 0.9402859586706311)
 assert(informationGain(df, 'Humidity') == 0.15183550136234159)
